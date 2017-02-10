d4a2768d9668e01172e28e0b2df36138d38edb16
Wen Bo Xie <wenbox323@gmail.com>
2017-02-10 23:02:35 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,5 @@
 function callMe() {
   var lyric = "maybe";
-  
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -13,17 +12,17 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-
   thisIsCrazy();
 }
 
 function sayMyName() {
   var name = "Cricky";
-  
-  function sayMy() {
-    var name = "Kristin";
-    console.log(name);
-  }
 
   sayMy();
+
+  function sayMy() {
+    var name = "Kristin";
+
+    console.log(name);
+  }
 }
