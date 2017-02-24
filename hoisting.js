1c37927b570f6493cdacfffc6705ac9784e9f52a
Rinat Aynulin <rinataynulin@gmail.com>
2017-02-24 19:21:05 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,5 @@
-function callMe() {
+  function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -9,22 +10,23 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  // fix the code in here:
+  thisIsCrazy();
+
+  
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  
 }
