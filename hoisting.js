1af93e58fa2e27aee3840ad7960bd786c6860526
Ri Xin Yang <rixin.yang.2001@gmail.com>
2019-12-23 18:29:30 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -22,8 +22,8 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 
   sayMy();
