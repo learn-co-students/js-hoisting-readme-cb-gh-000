db69380e23da0753df466fa544491178ab82156f
Sarmad Riaz <ms30237@gmail.com>
2020-07-06 21:26:09 +0000

    Done.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require_relative '../my_server.rb'
+require 'rack'
+require 'rack/test'
+
+def app() 
+  MyServer.new
+end
+
+RSpec.configure do |config|
+
+  config.include Rack::Test::Methods
+  config.order = 'default'
+end
+© 2020 GitHub, Inc.
